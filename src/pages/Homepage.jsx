13692e8fb7843f7869918457f1b9acea1559a2a2
Khadicha <246abdurashidova123@gmail.com>
2023-04-22 03:11:28 +0500

    Arrival section started

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,7 +6,12 @@ import mainPhone from "../assets/images/home-img-1.jpg";
 import banner1 from "../assets/images/banner-1.jpg";
 import banner2 from "../assets/images/banner-2.jpg";
 import banner3 from "../assets/images/banner-3.jpg";
-
+import arrival1 from "../assets/images/arrival-1.jpg";
+import arrival2 from "../assets/images/arrival-2.jpg";
+import arrival3 from "../assets/images/arrival-3.jpg";
+import arrival4 from "../assets/images/arrival-4.jpg";
+import arrival5 from "../assets/images/arrival-5.jpg";
+import arrival6 from "../assets/images/arrival-6.jpg";
 function Homepage() {
   return (
     <div>
@@ -23,8 +28,8 @@ function Homepage() {
       </div>
       {/* Phone section */}
 
-      {/* Offer section */}
-      <div className="offerSection">
+      <div className="secondSection">
+        {/* Offer section */}
         <div className="offerContainers">
           <div className="banner">
             <img src={banner1} alt="banner" />
@@ -48,8 +53,88 @@ function Homepage() {
             </div>
           </div>
         </div>
+        {/* Offer section */}
+
+        {/* New Arrivals section */}
+        <h2 className="arrivalsText">
+          New <span>Arrivals</span>
+        </h2>
+        <div>
+          <div className="arrivalContainer">
+            <img src={arrival1} alt="" />
+            <div className="arrivaltext">
+              <h3>HD television</h3>
+              <p>
+                $249.99{" "}
+                <span>
+                  <del>$399.99</del>
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="arrivalContainer">
+            <img src={arrival2} alt="" />
+            <h3>HD television</h3>
+            <p>
+              $249.99{" "}
+              <span>
+                <del>$399.99</del>
+              </span>
+            </p>
+          </div>
+          <div className="arrivalContainer">
+            <img src={arrival3} alt="" />
+            <h3>HD television</h3>
+            <p>
+              $249.99{" "}
+              <span>
+                <del>$399.99</del>
+              </span>
+            </p>
+          </div>
+          <div>
+            <img src={arrival4} alt="" />
+            <h3>HD television</h3>
+            <p>
+              $249.99{" "}
+              <span>
+                <del>$399.99</del>
+              </span>
+            </p>
+          </div>
+          <div>
+            <img src={arrival6} alt="" />
+            <h3>HD television</h3>
+            <p>
+              $249.99{" "}
+              <span>
+                <del>$399.99</del>
+              </span>
+            </p>
+          </div>
+          <div>
+            <img src={arrival5} alt="" />
+            <h3>HD television</h3>
+            <p>
+              $249.99{" "}
+              <span>
+                <del>$399.99</del>
+              </span>
+            </p>
+          </div>
+          <div>
+            <img src={arrival1} alt="" />
+            <h3>HD television</h3>
+            <p>
+              $249.99{" "}
+              <span>
+                <del>$399.99</del>
+              </span>
+            </p>
+          </div>
+        </div>
+        {/* New Arrivals section */}
       </div>
-      {/* Offer section */}
     </div>
   );
 }
