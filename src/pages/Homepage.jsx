f9b44a747b3bffc44fda40c0169ee9e2bc86282b
Khadicha <246abdurashidova123@gmail.com>
2023-04-22 02:37:51 +0500

    Offer section started

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,15 +3,10 @@ import Header from "../layouts/Header";
 
 import logo from "../assets/images/photo_2022-11-20_12-15-08-removebg-preview.png";
 import mainPhone from "../assets/images/home-img-1.jpg";
-// import mainPhone from "../assets/images/home-img-1.jpg";
-// import mainPhone from "../assets/images/home-img-1.jpg";
-// import mainPhone from "../assets/images/home-img-1.jpg";
-// import mainPhone from "../assets/images/home-img-1.jpg";
-// import mainPhone from "../assets/images/home-img-1.jpg";
-// import mainPhone from "../assets/images/home-img-1.jpg";
-// import mainPhone from "../assets/images/home-img-1.jpg";
-// import mainPhone from "../assets/images/home-img-1.jpg";
-// import mainPhone from "../assets/images/home-img-1.jpg";
+import banner1 from "../assets/images/banner-1.jpg";
+import banner2 from "../assets/images/banner-2.jpg";
+import banner3 from "../assets/images/banner-3.jpg";
+
 function Homepage() {
   return (
     <div>
@@ -27,6 +22,34 @@ function Homepage() {
         </div>
       </div>
       {/* Phone section */}
+
+      {/* Offer section */}
+      <div className="offerSection">
+        <div className="offerContainers">
+          <div className="banner">
+            <img src={banner1} alt="banner" />
+            <div className="bannerText">
+              <h2>Special Offer</h2>
+              <p>Upto 50% Off</p>
+            </div>
+          </div>
+          <div className="banner">
+            <img src={banner2} alt="banner" />
+            <div className="bannerText">
+              <h2>Special Offer</h2>
+              <p>Upto 50% Off</p>
+            </div>
+          </div>
+          <div className="banner">
+            <img src={banner3} alt="banner" />
+            <div className="bannerText">
+              <h2>Special Offer</h2>
+              <p>Upto 50% Off</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Offer section */}
     </div>
   );
 }
