cc24c43f5b004f2b40ca74724f9bc241988f7549
Khadicha <246abdurashidova123@gmail.com>
2023-04-22 23:06:49 +0500

    About page started

---
+++ src/pages/Aboutpage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../layouts/Header";
+import Footer from "../layouts/Footer";
+
+function About() {
+  return (
+    <div>
+      <Header />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default About;
