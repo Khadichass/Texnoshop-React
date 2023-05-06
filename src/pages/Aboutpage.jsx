e23c151912d37d80899f5093cc65d9cd9d39a20d
Khadicha <246abdurashidova123@gmail.com>
2023-05-06 09:43:57 +0500

    about page first part finished

--- src/pages/Aboutpage.jsx
+++ src/pages/Aboutpage.jsx
@@ -3,11 +3,34 @@ import React from "react";
 import Header from "../layouts/Header";
 import Footer from "../layouts/Footer";
 
+import about from "../assets/images/about-img.png";
 function About() {
   return (
     <div>
       <Header />
-      <div>About</div>
+      <div className="about">
+        <div className="firstpart">
+          <div className="firstpart_left">
+            <img src={about} alt="about " />
+          </div>
+          <div className="firstpart_right">
+            <h1>OUR STORY</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+              illo eos deserunt natus distinctio odio aspernatur minima alias
+              dolor ut. Explicabo natus ullam facilis consectetur quae possimus
+              ducimus similique, tempore quo animi omnis magnam.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+              illo eos deserunt natus distinctio odio aspernatur minima alias
+              dolor ut. Explicabo natus ullam facilis consectetur quae possimus
+              ducimus similique, tempore quo animi omnis magnam.
+            </p>
+            <button>Read More</button>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
