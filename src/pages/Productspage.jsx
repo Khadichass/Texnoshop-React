ef8d7a6de0d4ca83a9b821af850248e7488091d2
Khadicha <246abdurashidova123@gmail.com>
2023-05-07 23:15:06 +0500

    photos imported

--- src/pages/Productspage.jsx
+++ src/pages/Productspage.jsx
@@ -7,8 +7,8 @@ import product1 from "../assets/images/product-1.jpg";
 import product2 from "../assets/images/product-2.jpg";
 import product3 from "../assets/images/product-3.jpg";
 import product4 from "../assets/images/product-4.jpg";
-import product5 from "../assets/images/product-5.jpg";
-import product6 from "../assets/images/product-6.jpg";
+import product5 from "../assets/images/product-6.jpg";
+import product6 from "../assets/images/product-7.jpg";
 
 function Products() {
   return (
