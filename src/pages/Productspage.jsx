6a6c7e88576bb4d75a8f2a1160b25e4c92e499d3
Khadicha <246abdurashidova123@gmail.com>
2023-05-07 23:13:15 +0500

    product page div card created

--- src/pages/Productspage.jsx
+++ src/pages/Productspage.jsx
@@ -3,6 +3,13 @@ import React from "react";
 import Header from "../layouts/Header";
 import Footer from "../layouts/Footer";
 
+import product1 from "../assets/images/product-1.jpg";
+import product2 from "../assets/images/product-2.jpg";
+import product3 from "../assets/images/product-3.jpg";
+import product4 from "../assets/images/product-4.jpg";
+import product5 from "../assets/images/product-5.jpg";
+import product6 from "../assets/images/product-6.jpg";
+
 function Products() {
   return (
     <div>
@@ -11,7 +18,9 @@ function Products() {
         <h1 className="products_h1">
           Your <span>Products</span>
         </h1>
-        <div></div>
+        <div>
+          <div></div>
+        </div>
       </div>
       <Footer />
     </div>
