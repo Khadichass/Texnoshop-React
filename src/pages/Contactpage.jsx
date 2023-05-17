6b0fb20b5bc5320e36fba193aad0e9d4b30a6b65
Khadicha <246abdurashidova123@gmail.com>
2023-05-17 21:33:39 +0500

    in first section 3 div cards added

--- src/pages/Contactpage.jsx
+++ src/pages/Contactpage.jsx
@@ -8,6 +8,22 @@ function Contact() {
     <div>
       <Header />
       <div>
+        <div>
+          <div>
+            <h2>Address</h2>
+            <p>Mumbai, India - 400104</p>
+          </div>
+          <div>
+            <h2>Email</h2>
+            <p>Shaikhanas@Gmail.Com</p>
+            <p>Anasbhai@Gmail.Com</p>
+          </div>
+          <div>
+            <h2>Number</h2>
+            <p>+123-456-7890</p>
+            <p>+111-222-3333</p>
+          </div>
+        </div>
         <div></div>
         <div></div>
       </div>
